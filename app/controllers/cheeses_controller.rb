a9cbf229134796fa1654b35b6cef254109c5f19c
Rlynette <lynette.ronnoh@student.moringaschool.com>
2022-11-23 14:37:13 +1100

    Completed assignment

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,14 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+      render json: cheeses, except: [:created_at, :updated_at]
+    # render json: cheeses, only: [:id, :name, :price, :is_best_seller]
   end
 
   # GET /cheeses/:id
-  def show
-    cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
-  end
-
+ 
+def show
+  cheese = Cheese.find_by(id: params[:id])
+  render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+end
 end
